4171f312fbda23839784980ccb30198f23c00f48
bpillet <bpillet@users.noreply.github.com>
2016-02-02 11:15:00 +0100

    hypercohomologie

---
+++ Cohomologie/Spectral sequences/auto/_region_.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "amsdip"
+     ""
+     "latex2e"
+     "amsart11"
+     "amsart"
+     "makeidx"
+     "draft"
+     "11pt")))
+
